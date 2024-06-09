bee785215044a86c29b5a2da2dac63bef1919d06
Igor <114025058+igaryakqwe@users.noreply.github.com>
2024-06-10 00:06:10 +0300

    Add typography

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,20 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontSize: {
+        'h1': '36px',
+        'm-h1': '18px',
+        'p': '16px',
+        'l-p': '14px',
+        'm-p': '12px',
+        'p-light': '10px',
+      },
+      fontWeight: {
+        'light': '300',
+        'regular': '400',
+        'semibold': '600',
+      },
+
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
