8ad153e154999aad6e4f61f079861afe51655ee8
Ihor <114025058+igaryakqwe@users.noreply.github.com>
2024-07-11 11:10:36 +0300

    Add Footer (#39)
    * Add footer
    
    - added basic footer
    - removed boilerplate code from main page
    
    * Add responsive for footer

---
+++ components/footer/components/footer-media.tsx
@@ -0,0 +1,33 @@
+import { FC } from 'react';
+import Link from 'next/link';
+
+import { Item } from '@/components/footer/types';
+import { cn } from '@/lib/cn';
+
+interface FooterMediaProps {
+  items: Item[];
+  className?: string;
+}
+
+const FooterMedia: FC<FooterMediaProps> = ({ items, className = '' }) => {
+  return (
+    <div className={cn(className, 'align-bottom self-start')}>
+      <p className="font-m-button text-m-p mb-[20px] lg:hidden">
+        Соціальні мережі
+      </p>
+      <div className="flex gap-[23px]">
+        {items.map((item: Item) => (
+          <Link
+            className="size-[15px] md:size-[20px]"
+            key={item.href}
+            href={item.href}
+          >
+            {item.icon}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default FooterMedia;
---
+++ components/footer/footer.tsx
@@ -0,0 +1,50 @@
+import { CopyrightIcon } from 'lucide-react';
+import Image from 'next/image';
+
+import FooterColumn from '@/components/footer/components/footer-column';
+import FooterMedia from '@/components/footer/components/footer-media';
+import {
+  mainItems,
+  mediaItems,
+  otherItems,
+  supportItems,
+} from '@/components/footer/constants';
+
+const Footer = () => {
+  return (
+    <footer className="p-[20px] md:p-[32px] lg:p-[64px] xl:p-[100px]">
+      <div className="flex align-top justify-between flex-wrap">
+        <div className="w-screen xl:w-auto mb-[20px]">
+          <Image
+            className="object-cover align-top w-[67px] md:w-[100px] lg:w-[200px] h-auto"
+            src="/images/Logo.png"
+            alt="logo"
+            width={200}
+            height={73}
+          />
+        </div>
+        <FooterColumn name="Основні сторінки" items={mainItems} />
+        <FooterColumn name="Підтримка" items={supportItems} />
+        <div className="hidden lg:flex lg:flex-col">
+          <FooterColumn items={otherItems} />
+          <FooterMedia items={mediaItems} />
+        </div>
+        <FooterColumn className="block lg:hidden" items={otherItems} />
+        <FooterMedia className="block lg:hidden" items={mediaItems} />
+        <Image
+          className="md:self-end self-center size-[100px] md:size-[200px]"
+          src="/images/qrcode.png"
+          alt="qrcode"
+          width={200}
+          height={200}
+        />
+      </div>
+      <div className="flex gap-[9px] items-center mt-[10px]">
+        <CopyrightIcon className="size-[15px] md:size-[20px]" />
+        <p className="font-p text-m-p md:text-p">2024 Студентська рада КПІ</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
