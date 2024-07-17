b36fbaaa8d4cfb354edde56eaf15d0847aec07bc
StepanPotiienko <138608284+StepanPotiienko@users.noreply.github.com>
2024-07-17 17:53:45 +0300

    Added breadcrumbs, categories and accordion

--- app/(client)/faq/page.tsx
+++ app/(client)/faq/page.tsx
@@ -1,9 +1,122 @@
-import { FAQAccordion } from '@/components/ui/accordion';
+import { Cable, Layers } from 'lucide-react';
 
-export default async function FAQ() {
+import { FAQAccordion } from '@/components/ui/accordion';
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb';
+import { Button } from '@/components/ui/button';
+
+export default function FAQ() {
   return (
-    <>
-      <FAQAccordion />
-    </>
+    <header className="w-full flex flex-col">
+      <Breadcrumbs />
+      <main className="flex lg:flex-row md:flex-col sm:flex-col justify-evenly just">
+        <AsideCategories />
+        <FAQAccordion />
+      </main>
+    </header>
   );
 }
+
+const Breadcrumbs = () => {
+  return (
+    <Breadcrumb className="w-1/10 h-20 flex flex-row">
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <Layers /> Головна
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbLink>
+            <Cable /> Часті питання
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+};
+
+const AsideCategories = () => {
+  return (
+    <aside className="w-1/11 h-screen flex">
+      <ul className="w-full flex flex-col gap-1">
+        <li>
+          <Button
+            variant={'outline'}
+            size={'sm'}
+            className="w-full not-italic font-normal leading-normal text-base"
+          >
+            Загальні питання
+          </Button>
+        </li>
+        <li>
+          <Button
+            variant={'outline'}
+            size={'sm'}
+            className="w-full not-italic font-normal leading-normal text-base"
+          >
+            Отримати документи
+          </Button>
+        </li>
+        <li>
+          <Button
+            variant={'outline'}
+            size={'sm'}
+            className="w-full not-italic font-normal leading-normal text-base"
+          >
+            Головна інформація
+          </Button>
+        </li>
+        <li>
+          <Button
+            variant={'outline'}
+            size={'sm'}
+            className="w-full not-italic font-normal leading-normal text-base"
+          >
+            Питання до студради
+          </Button>
+        </li>
+        <li>
+          <Button
+            variant={'outline'}
+            size={'sm'}
+            className="w-full not-italic font-normal leading-normal text-base "
+          >
+            Питання студентам
+          </Button>
+        </li>
+        <li>
+          <Button
+            variant={'outline'}
+            size={'sm'}
+            className="w-full not-italic font-normal leading-normal text-base "
+          >
+            Поширенні запитання
+          </Button>
+        </li>
+        <li>
+          <Button
+            variant={'outline'}
+            size={'sm'}
+            className="w-full not-italic font-normal leading-normal text-base"
+          >
+            Питання про вступ
+          </Button>
+        </li>
+        <li>
+          <Button
+            variant={'outline'}
+            size={'sm'}
+            className="w-full not-italic font-normal leading-normal text-base "
+          >
+            Організаційні питання
+          </Button>
+        </li>
+      </ul>
+    </aside>
+  );
+};
