d7b3ef906a3aba02756fad373f97c06a949d509c
Markusplay <mshilo2004@gmail.com>
2024-07-24 20:23:52 +0300

    added swr to register page

--- types/group.interface.ts
+++ types/group.interface.ts
@@ -3,3 +3,19 @@ export interface IGroupData {
   name: string;
   faculty: string;
 }
+
+export interface IGroupsResponse {
+  data: IGroupData[];
+  paging: {
+    pageCount: number;
+    totalItemCount: number;
+    pageNumber: number;
+    pageSize: number;
+    hasPreviousPage: boolean;
+    hasNextPage: boolean;
+    isFirstPage: boolean;
+    isLastPage: boolean;
+    firstItemOnPage: number;
+    lastItemOnPage: number;
+  };
+}
