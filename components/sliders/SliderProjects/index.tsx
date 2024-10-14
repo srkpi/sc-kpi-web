818e6e1c4c441db15e8aaf7ea4f8413e9d85c616
Kyryloloshka <kirill.contrmptuous@gmail.com>
2024-10-14 04:05:31 +0300

    bug/fix-slider-images-dublicating

--- components/sliders/SliderProjects/index.tsx
+++ components/sliders/SliderProjects/index.tsx
@@ -19,7 +19,7 @@ import '@/components/sliders/styles.css';
 const SliderProjects = ({ projects }: { projects: DepartmentProject[] }) => {
   const [nav1, setNav1] = useState<Slider | null>(null);
   const [nav2, setNav2] = useState<Slider | null>(null);
-
+  const minProjectsToMakeComplexSlider = 3;
   const next = () => {
     if (!nav1 || !nav2) return;
     nav1.slickNext();
@@ -52,7 +52,8 @@ const SliderProjects = ({ projects }: { projects: DepartmentProject[] }) => {
 
   const settingsForSlider2 = {
     className: 'main-slider',
-    slidesToShow: 3,
+    slidesToShow: projects.length < minProjectsToMakeComplexSlider ? 1 : 3,
+    fade: projects.length < minProjectsToMakeComplexSlider,
     responsive: [
       {
         breakpoint: 1536,
@@ -67,7 +68,9 @@ const SliderProjects = ({ projects }: { projects: DepartmentProject[] }) => {
     return <SkeletonDepartmentCard />;
   }
   return (
-    <div className="relative md:flex">
+    <div
+      className={`relative md:flex ${projects.length < minProjectsToMakeComplexSlider ? 'justify-center' : ''}`}
+    >
       <div className="md:w-[45%] 2xl:w-[40%] slider-container md:flex md:flex-col">
         <Slider
           asNavFor={nav2 ? nav2 : undefined}
@@ -80,6 +83,7 @@ const SliderProjects = ({ projects }: { projects: DepartmentProject[] }) => {
             <React.Fragment key={index}>
               <DepartmentCardText project={project} index={index} />
               <DepartmentMoblieCard
+                isArrowsShown={projects.length > 1}
                 project={project}
                 next={next}
                 prev={previous}
@@ -89,8 +93,12 @@ const SliderProjects = ({ projects }: { projects: DepartmentProject[] }) => {
           ))}
         </Slider>
       </div>
-      <div className="hidden md:block md:w-[55%] 2xl:w-[2300px] slider-container relative">
-        <div className="absolute hidden 2xl:block right-[-3px] top-0 h-full w-[500px] bg-gradient-to-r from-transparent to-dark to-80% z-10"></div>
+      <div
+        className={`hidden md:block md:w-[55%] ${projects.length >= minProjectsToMakeComplexSlider ? '2xl:w-[2300px] ' : 'max-w-[720px]'} slider-container relative`}
+      >
+        {projects.length >= minProjectsToMakeComplexSlider && (
+          <div className="absolute hidden 2xl:block right-[-3px] top-0 h-full w-[500px] bg-gradient-to-r from-transparent to-dark to-80% z-10"></div>
+        )}
         <Slider
           asNavFor={nav1 ? nav1 : undefined}
           ref={slider2 => {
@@ -99,19 +107,26 @@ const SliderProjects = ({ projects }: { projects: DepartmentProject[] }) => {
           {...generalSettingsSlider}
           {...settingsForSlider2}
         >
-          {projects.map((project: DepartmentProject) => (
-            <React.Fragment key={project.id}>
-              <DepartmentCardPicture project={project} />
+          {projects.map((project: DepartmentProject, index: number) => (
+            <React.Fragment key={index}>
+              <DepartmentCardPicture
+                project={project}
+                isMarginRight={
+                  projects.length >= minProjectsToMakeComplexSlider
+                }
+              />
             </React.Fragment>
           ))}
         </Slider>
-        <div className="flex gap-1 self-end absolute bottom-0 left-0 z-10 px-8 py-5 origin-bottom-left scale-90 lg:scale-100">
-          <ArrowLeft onClick={previous} />
-          <ArrowRight onClick={next} />
-          <div className="absolute bottom-[1px] left-[-3px]">
-            <IconBottomLeftCornerCur />
+        {projects.length > 1 && (
+          <div className="flex gap-1 self-end absolute bottom-0 left-0 z-10 px-8 py-5 origin-bottom-left scale-90 lg:scale-100">
+            <ArrowLeft onClick={previous} />
+            <ArrowRight onClick={next} />
+            <div className="absolute bottom-[1px] left-[-3px]">
+              <IconBottomLeftCornerCur />
+            </div>
           </div>
-        </div>
+        )}
         <div className="absolute h-full w-[2px] bg-dark bottom-0 left-[0px]"></div>
       </div>
     </div>
