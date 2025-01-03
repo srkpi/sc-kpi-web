71f3c7f27d84c6700136659980602aa3c2b736ac
Halyamov Kyrylo <117082241+Kyryloloshka@users.noreply.github.com>
2025-01-03 22:14:28 +0200

    Fix ssr home page (#187)
    * bug: make home page server side rendered
    
    * bug: make home page server side rendered #2

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -2,6 +2,8 @@ import dynamic from 'next/dynamic';
 
 const HomeComponent = dynamic(() => import('@/components/dynamic-pages/home/'));
 
+export const dynamic = 'force-dynamic';
+
 export default async function Home() {
   return <HomeComponent />;
 }
