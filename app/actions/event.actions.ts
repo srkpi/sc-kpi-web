bca77007b66c8269428873bf3b4cba948edbd407
Markusplay <mshilo2004@gmail.com>
2025-02-07 17:59:15 +0200

    remove events from website

--- app/actions/event.actions.ts
+++
@@ -1,7 +0,0 @@
-import { apiClient } from '@/lib/client';
-import { CalendarEvent } from '@/types/calendar-event';
-
-export async function getEventList() {
-  const res = await apiClient<CalendarEvent[]>('/calendar/event');
-  return res.data;
-}
