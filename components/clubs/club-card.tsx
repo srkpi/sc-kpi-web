8861330d0cd029ea91422b7b8d039238f60ea7ff
Markusplay <mshilo2004@gmail.com>
2025-02-13 01:30:40 +0200

    club image hot fix

--- components/clubs/club-card.tsx
+++ components/clubs/club-card.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 import { Button } from '@/components/ui/button';
@@ -8,25 +9,27 @@ interface ClubCardProps {
 }
 
 const ClubCard: React.FC<ClubCardProps> = ({ club }) => {
-  const { id, name, category, description, image, buttonLink } = club;
+  const { id, name, category, shortDescription, image, buttonLink } = club;
   return (
-    <div className="rounded-lg w-[290px] md:w-[400px] h-[555px] shadow-lg flex flex-col justify-center">
+    <div className="rounded-lg w-[290px] md:w-[400px] md:h-[555px] shadow-lg flex flex-col justify-center">
       <div className="relative w-full h-[300px] md:h-[338px] bg-muted rounded-lg flex items-center justify-center">
-        {category && (
-          <span className="absolute top-2 left-6 font-m-button text-white bg-blue bg-opacity-75 flex items-center justify-center px-3 py-3 md:px-5 md:py-3 rounded text-m-p md:text-p h-[38px] md:h-[48px]">
-            {category.toLocaleLowerCase()}
-          </span>
-        )}
-        <img
-          src={image || '/placeholder.png'}
-          alt={name}
+        <span className="absolute top-2 left-6 font-m-button text-white bg-blue bg-opacity-75 flex items-center justify-center px-3 py-3 md:px-5 md:py-3 rounded text-m-p md:text-p h-[38px] md:h-[48px]">
+          {category.toLocaleLowerCase()}
+        </span>
+        <Image
+          // className="w-full h-auto rounded-[20px] aspect-video object-cover order-first lg:order-last"
           className="w-[290px] h-[300px] md:w-[408px] md:h-[338px] object-cover rounded-lg"
+          src={image}
+          alt="image"
+          quality={100}
+          width={725}
+          height={414}
         />
       </div>
       <h2 className="font-m-h1 md:font-h3 mt-4">{name}</h2>
-      <p className="font-p md:font-m-p mt-2 line-clamp-4">{description}</p>
-      <div className="flex justify-between mt-auto space-x-0 md:space-x-4">
-        <Link href={`${buttonLink}`}>
+      <p className="font-p md:font-m-p mt-2 line-clamp-5">{shortDescription}</p>
+      <div className="flex justify-between mt-4 md:mt-auto space-x-0 md:space-x-4">
+        <Link href={buttonLink}>
           <Button
             variant="default"
             className="w-[130px] h-[40px] md:h-[50px] md:w-[197px] md:rounded-[6px]"
