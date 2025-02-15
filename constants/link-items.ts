90b8000a998fbd8930f6cc7a6dcae543db3b6ba8
Markusplay <mshilo2004@gmail.com>
2025-02-16 01:06:18 +0200

    add analytics url

--- constants/link-items.ts
+++ constants/link-items.ts
@@ -10,6 +10,10 @@ export const linkItems: LinkItem[] = [
     href: '/clubs',
   },
   {
+    name: 'Аналітика',
+    href: process.env.NEXT_PUBLIC_ANALYTICS_URL || '#',
+  },
+  {
     name: 'FAQ',
     href: '/faq',
   },
