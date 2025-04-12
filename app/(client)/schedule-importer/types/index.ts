47ea2efc1e35cf288e241141271d03dfde0e9a06
dmytro-mar <marchuk.dmytro1@lll.kpi.ua>
2025-04-12 23:50:27 +0300

    campus api requests fix (#283)
    * removed paging from campus api
    
    * removed unused import

--- app/(client)/schedule-importer/types/index.ts
+++ app/(client)/schedule-importer/types/index.ts
@@ -1,30 +1,6 @@
-import { Group } from '@/types/group';
 import { ScheduleEvent } from '@/types/schedule-event';
 
 export type WeekType = 'first' | 'second';
-
-export interface GroupsResponse {
-  paging: Paging;
-  data: Group[];
-}
-
-export interface Paging {
-  pageCount: number;
-  totalItemCount: number;
-  pageNumber: number;
-  pageSize: number;
-  hasPreviousPage: boolean;
-  hasNextPage: boolean;
-  isFirstPage: boolean;
-  isLastPage: boolean;
-  firstItemOnPage: number;
-  lastItemOnPage: number;
-}
-
-export interface EventsResponse {
-  paging: Paging | null;
-  data: EventsData;
-}
 
 export interface EventsData {
   groupCode: string;
