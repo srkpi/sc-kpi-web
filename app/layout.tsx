e9714bc103c3b3afc425286f1b3bf9c9d8ace045
Mariia Hupalenko <marichka.hupalenko@gmail.com>
2025-04-29 18:53:14 +0300

    Added OG image (#311)

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,16 @@ export const metadata: Metadata = {
     title,
     description,
     type: 'website',
-    images: '',
+    images: [
+      {
+        url: 'https://sc.kpi.ua/images/Logo.png',
+        secureUrl: 'https://sc.kpi.ua/images/Logo.png',
+        type: 'image/png',
+        width: 1200,
+        height: 630,
+        alt: "Preview image for Student's Council website",
+      },
+    ],
     url: 'https://sc.kpi.ua',
   },
 };
