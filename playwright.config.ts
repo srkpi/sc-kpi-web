a62123b950e845a8ab43f2efddd63076db1d7564
Dmytro Marchuk <marchuk.dmytro1@lll.kpi.ua>
2025-05-05 10:02:37 +0300

    added build step before testing

--- playwright.config.ts
+++ playwright.config.ts
@@ -41,9 +41,8 @@ export default defineConfig({
     },
   ],
 
-  /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run dev',
+    command: 'npm run build && npm run start',
     url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
   },
