73c01d8429174453761659dbdcc66ca6f882bc81
Bielikov Maksym <maximax6767@gmail.com>
2026-01-10 20:37:55 +0200

    Add default Posthog key

--- app/providers.tsx
+++ app/providers.tsx
@@ -6,12 +6,16 @@ import { PostHogProvider as PHProvider } from 'posthog-js/react';
 
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
-    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
-      api_host:
-        process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://eu.i.posthog.com',
-      person_profiles: 'always',
-      defaults: '2025-11-30',
-    });
+    posthog.init(
+      process.env.NEXT_PUBLIC_POSTHOG_KEY ||
+        'phc_SyMCx6wcYBDpb1Sn0dCzL5JsOu1iC5Kmi4C8Wt6aFrv',
+      {
+        api_host:
+          process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://eu.i.posthog.com',
+        person_profiles: 'always',
+        defaults: '2025-11-30',
+      },
+    );
   }, []);
 
   return <PHProvider client={posthog}>{children}</PHProvider>;
