898cbad6601f31da7430e5c4e18e3c504145c3fa
Bielikov Maksym <maximax6767@gmail.com>
2026-01-10 21:11:14 +0200

    Add reverse proxy for Posthog

--- app/providers.tsx
+++ app/providers.tsx
@@ -7,7 +7,8 @@ import { PostHogProvider as PHProvider } from 'posthog-js/react';
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
-      api_host:
+      api_host: '/ph',
+      ui_host:
         process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://eu.i.posthog.com',
       person_profiles: 'always',
       defaults: '2025-11-30',
--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: false,
   output: 'standalone',
+  skipTrailingSlashRedirect: true,
   images: {
     qualities: [100],
     remotePatterns: [
