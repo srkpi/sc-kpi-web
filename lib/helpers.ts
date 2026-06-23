560635ceddef97fb537528bcad9db2bfa36c13b6
Bielikov Maksym <maximax6767@gmail.com>
2026-06-23 11:00:41 +0300

    Random departments and projects order

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -1,3 +1,14 @@
 export const formatProjectIndex = (index: number) => {
   return index < 10 ? `0${index + 1}` : index + 1;
 };
+
+export const shuffle = <T>(array: T[]): T[] => {
+  const shuffled = [...array];
+
+  for (let i = shuffled.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+  }
+
+  return shuffled;
+};
